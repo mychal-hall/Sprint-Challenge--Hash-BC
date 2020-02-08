da25d1e95da365e1635fd4e7a53a61e7517d7958
Mychal Hall <hallmychal@gmail.com>
2020-02-08 13:44:16 -0600

    Blockchain (DONE)

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,7 +24,8 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    while valid_proof(last_proof, proof) is False:
+    last_hash = hashlib.sha256(f'{last_proof}'.encode()).hexdigest()
+    while valid_proof(last_hash, proof) is False:
         proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
@@ -41,9 +42,10 @@ def valid_proof(last_hash, proof):
     """
 
     guess = f'{proof}'.encode()
-    guess_hash = hashlib.sha256(guess).hexdigest()
-    return guess_hash[:6] == last_hash[-6:]
+    hash_guess = hashlib.sha256(guess).hexdigest()
 
+    # validate if your next proof hash - first 6 characters - matches last hash - last 6 characters
+    return hash_guess[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
